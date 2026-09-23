df5131bc07c9aaac4c196d615043584ba29b9838
ravi-kaliya <108667836+ravi-kaliya@users.noreply.github.com>
2026-09-23 17:03:06 +0000

    footer p added

--- src/components/Invitation.tsx
+++ src/components/Invitation.tsx
@@ -614,6 +614,7 @@ Join us for an unforgettable evening filled with laughter, music, and togetherne
 
       {/* ================= FOOTER ================= */}
       <footer className="iv-footer">
+        <p className="iv-foot-date">With blessings of Almighty  God and our respected father <br /> Sh sham Sunder Jindal</p>
         <img
           src={srLogoDark}
           alt="wax seal"
