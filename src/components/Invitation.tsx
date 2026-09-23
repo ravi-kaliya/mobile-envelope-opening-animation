c07f3260d712ac49ee1b8efb27a01f2cfaaa754d
ravi-kaliya <108667836+ravi-kaliya@users.noreply.github.com>
2026-09-23 17:32:03 +0000

    3line updated

--- src/components/Invitation.tsx
+++ src/components/Invitation.tsx
@@ -614,7 +614,7 @@ Join us for an unforgettable evening filled with laughter, music, and togetherne
 
       {/* ================= FOOTER ================= */}
       <footer className="iv-footer">
-        <p className="iv-foot-date">With the blessings of Almighty God & the love of our respected father Sh Sham  Sunder Jindal</p>
+        <p className="iv-foot-date">With the blessings of Almighty God & the love of our respected <br/>father Sh Sham  Sunder Jindal</p>
         <img
           src={srLogoDark}
           alt="wax seal"
